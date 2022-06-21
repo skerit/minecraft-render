a4b4813a859016e37027ed3298d8ea8fcd64c542
Jelle De Loecker <jelle@elevenways.be>
2022-06-21 19:39:28 +0200

    Add external jar support

--- src/minecraft.ts
+++ src/minecraft.ts
@@ -5,143 +5,272 @@ import { Jar } from './utils/jar'
 import type { AnimationMeta, BlockModel, Renderer, RendererOptions } from './utils/types'
 
 export class Minecraft {
-   protected jar: Jar
-   protected renderer!: Renderer | null
-   protected _cache: { [key: string]: any } = {}
+	protected jar: Jar
+	protected renderer!: Renderer | null
+	protected _cache: { [key: string]: any } = {}
+	protected _modid_jars : { [key: string]: Jar } = {}
 
-   protected constructor(
-      public file: string | Jar,
-      protected readonly defaultNamespace = 'minecraft'
-   ) {
-      if (file instanceof Jar) {
-         this.jar = file
-      } else {
-         this.jar = Jar.open(file)
-      }
-   }
+	/**
+	 * Construct the Minecraft instance
+	 * 
+	 * @author   Jelle De Loecker   <jelle@elevenways.be>
+	 * @since    1.2.0
+	 * @version  1.2.0
+	 * 
+	 * @param   file        The path to the jar, or the jar itself
+	 * @param   namespace   The default namespace used in thie jar
+	 */
+	protected constructor(public file: string | Jar, protected readonly defaultNamespace = 'minecraft') {
+		if (file instanceof Jar) {
+			this.jar = file
+		} else {
+			this.jar = Jar.open(file)
+		}
+	}
 
-   protected id(name: string) {
-      if (name.includes(':')) {
-         const [namespace, id] = name.split(':')
-         return { namespace, id }
-      } else {
-         return { namespace: 'minecraft', id: name }
-      }
-   }
+	/**
+	 * Create an identifier
+	 */
+	protected id(name: string) {
+		if (name.includes(':')) {
+			const [namespace, id] = name.split(':')
+			return { namespace, id }
+		} else {
+			return { namespace: 'minecraft', id: name }
+		}
+	}
 
-   static open(file: string | Jar, namespace?: string) {
-      return new Minecraft(file, namespace)
-   }
+	static open(file: string | Jar, namespace?: string) {
+		return new Minecraft(file, namespace)
+	}
 
-   async getBlockNameList(namespace = this.defaultNamespace): Promise<string[]> {
-      return (await this.jar.entries(`assets/${namespace}/models/block`))
-         .filter(entry => entry.name.endsWith('.json'))
-         .map(
-            entry =>
-               namespace +
-               ':' +
-               entry.name.slice(`assets/${namespace}/models/block/`.length, -'.json'.length)
-         )
-   }
+	async getBlockNameList(namespace = this.defaultNamespace): Promise<string[]> {
+		return (await this.jar.entries(`assets/${namespace}/models/block`))
+			.filter(entry => entry.name.endsWith('.json'))
+			.map(
+				entry =>
+					namespace +
+					':' +
+					entry.name.slice(`assets/${namespace}/models/block/`.length, -'.json'.length)
+			)
+	}
 
-   async getBlockList(namespace = this.defaultNamespace): Promise<BlockModel[]> {
-      return await Promise.all(
-         (await this.getBlockNameList(namespace)).map(block => this.getModel(block))
-      )
-   }
+	/**
+	 * Register other jars
+	 * 
+	 * @author   Jelle De Loecker   <jelle@elevenways.be>
+	 * @since    1.2.0
+	 * @version  1.2.0
+	 * 
+	 * @param   modid   The modid of the other jar
+	 * @param   path    The path of the other jar
+	 */
+	async registerModId(modid: string, path : string) {
+		this._modid_jars[modid] = Jar.open(path);
+	}
 
-   async getModelFile<T = BlockModel>(name = 'block/block'): Promise<T> {
-      let { namespace, id } = this.id(name)
+	/**
+	 * Get a file from any of the registered jars
+	 * 
+	 * @author   Jelle De Loecker   <jelle@elevenways.be>
+	 * @since    1.2.0
+	 * @version  1.2.0
+	 * 
+	 * @param    path    The path of the file to read
+	 */
+	async getFile(path : string) : Promise<Buffer | null> {
 
-      if (id.indexOf('/') == -1) {
-         id = `block/${id}`
-      }
+		let result;
 
-      const path = `assets/${namespace}/models/${id}.json`
+		// Try our jar first
+		try {
+			result = await this.jar.read(path);
+		} catch (err) {
+			// Ignore
+		}
 
-      try {
-         if (this._cache[path]) {
-            return JSON.parse(JSON.stringify(this._cache[path]))
-         }
+		if (result) {
+			return result;
+		}
 
-         this._cache[path] = await this.jar.readJson(path)
+		for (let key in this._modid_jars) {
+			let jar = this._modid_jars[key];
 
-         return this._cache[path]
-      } catch (e) {
-         throw new Error(`Unable to find model file: ${path}`)
-      }
-   }
+			try {
+				result = await jar.read(path);
+			} catch (err) {
+				continue;
+			}
 
-   async getTextureFile(name: string = '') {
-      const { namespace, id } = this.id(name)
+			if (result) {
+				return result;
+			}
+		}
 
-      const path = `assets/${namespace}/textures/${id}.png`
+		return null;
+	}
 
-      try {
-         return await this.jar.read(path)
-      } catch (e) {
-         throw new Error(`Unable to find texture file: ${path}`)
-      }
-   }
+	/**
+	 * Get json data from any of the registered jars
+	 * 
+	 * @author   Jelle De Loecker   <jelle@elevenways.be>
+	 * @since    1.2.0
+	 * @version  1.2.0
+	 * 
+	 * @param    path    The path of the file to read
+	 */
+	async getParsedJson(path : string) : Promise<any> {
 
-   async getTextureMetadata(name: string = ''): Promise<AnimationMeta | null> {
-      const { namespace, id } = this.id(name)
+		if (!this._cache[path]) {
+			let data = await this.getFile(path);
 
-      const path = `assets/${namespace}/textures/${name}.png.mcmeta`
+			if (data) {
+				this._cache[path] = this.parseJson(data);
+			}
+		}
 
-      try {
-         return await this.jar.readJson(path)
-      } catch (e) {
-         return null
-      }
-   }
+		if (this._cache[path]) {
+			return this.clone(this._cache[path]);
+		}
+	 }
 
-   async *render(blocks: BlockModel[], options?: RendererOptions) {
-      try {
-         await this.prepareRenderEnvironment(options)
+	/**
+	 * Parse or clone json data
+	 * 
+	 * @author   Jelle De Loecker   <jelle@elevenways.be>
+	 * @since    1.2.0
+	 * @version  1.2.0
+	 * 
+	 * @param    path    The path of the file to read
+	 */
+	public parseJson(data : string | Object | Buffer) : Object | null {
 
-         for (const block of blocks) {
-            yield await render(this, block)
-         }
-      } finally {
-         await this.cleanupRenderEnvironment()
-      }
-   }
+		let str : string;
 
-   async renderSingle(block: BlockModel) {
-      return await render(this, block)
-   }
+		if (typeof data == 'object') {
 
-   async getModel(blockName: string): Promise<BlockModel> {
-      let { parent, ...model } = await this.getModelFile(blockName)
+			if (Buffer.isBuffer(data)) {
+				str = data.toString('utf8');
+			} else {
+				str = JSON.stringify(data);
+			}
+		} else if (typeof data == 'string') {
+			str = data;
+		} else {
+			return null;
+		}
 
-      if (parent) {
-         model = deepAssign({}, await this.getModel(parent), model)
+		return JSON.parse(str);
+	}
 
-         if (!model.parents) {
-            model.parents = []
-         }
+	/**
+	 * Clone the given input
+	 * 
+	 * @author   Jelle De Loecker   <jelle@elevenways.be>
+	 * @since    1.2.0
+	 * @version  1.2.0
+	 * 
+	 * @param    path    The path of the file to read
+	 */
+	 public clone<T>(input : T) : T {
+		return JSON.parse(JSON.stringify(input));
+	 }
 
-         model.parents.push(parent)
-      }
+	/**
+	 * Get a list of all blocks of the given namespace
+	 */
+	async getBlockList(namespace = this.defaultNamespace): Promise<BlockModel[]> {
+		return await Promise.all(
+			(await this.getBlockNameList(namespace)).map(block => this.getModel(block))
+		)
+	}
 
-      return deepAssign(model, { blockName })
-   }
+	async getModelFile<T = BlockModel>(name = 'block/block'): Promise<T> {
+		let { namespace, id } = this.id(name)
 
-   async close() {
-      await this.jar.close()
-   }
+		if (id.indexOf('/') == -1) {
+			id = `block/${id}`
+		}
 
-   async prepareRenderEnvironment(options: RendererOptions = {}) {
-      this.renderer = await prepareRenderer(options)
-   }
+		const path = `assets/${namespace}/models/${id}.json`
 
-   async cleanupRenderEnvironment() {
-      await destroyRenderer(this.renderer!)
-      this.renderer = null
-   }
+		try {
+			return this.getParsedJson(path);
+		} catch (e) {
+			throw new Error(`Unable to find model file: ${path}`)
+		}
+	}
 
-   getRenderer() {
-      return this.renderer!
-   }
+	async getTextureFile(name: string = '') {
+		const { namespace, id } = this.id(name)
+
+		const path = `assets/${namespace}/textures/${id}.png`
+
+		try {
+			return await this.getFile(path)
+		} catch (e) {
+			throw new Error(`Unable to find texture file: ${path}`)
+		}
+	}
+
+	async getTextureMetadata(name: string = ''): Promise<AnimationMeta | null> {
+		const { namespace, id } = this.id(name)
+
+		const path = `assets/${namespace}/textures/${name}.png.mcmeta`
+
+		try {
+			return await this.getParsedJson(path);
+		} catch (e) {
+			return null
+		}
+	}
+
+	async *render(blocks: BlockModel[], options?: RendererOptions) {
+		try {
+			await this.prepareRenderEnvironment(options)
+
+			for (const block of blocks) {
+				yield await render(this, block)
+			}
+		} finally {
+			await this.cleanupRenderEnvironment()
+		}
+	}
+
+	async renderSingle(block: BlockModel) {
+		return await render(this, block)
+	}
+
+	async getModel(blockName: string): Promise<BlockModel> {
+		let { parent, ...model } = await this.getModelFile(blockName)
+
+		if (parent) {
+			model = deepAssign({}, await this.getModel(parent), model)
+
+			if (!model.parents) {
+				model.parents = []
+			}
+
+			model.parents.push(parent)
+		}
+
+		return deepAssign(model, { blockName })
+	}
+
+	async close() {
+		await this.jar.close()
+	}
+
+	async prepareRenderEnvironment(options: RendererOptions = {}) {
+		this.renderer = await prepareRenderer(options)
+	}
+
+	async cleanupRenderEnvironment() {
+		await destroyRenderer(this.renderer!)
+		this.renderer = null
+	}
+
+	getRenderer() {
+		return this.renderer!
+	}
 }
